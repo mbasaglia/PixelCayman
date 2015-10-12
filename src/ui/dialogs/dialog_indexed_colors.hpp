586c41b9654518090de289742aa6681b7325b8a7
Mattia Basaglia <mattia.basaglia@gmail.com>
2015-10-12 19:10:00 +0100

    Design the dialog used to switch to indexed colors

---
+++ src/ui/dialogs/dialog_indexed_colors.hpp
@@ -0,0 +1,51 @@
+/**
+ * \file
+ *
+ * \author Mattia Basaglia
+ *
+ * \copyright Copyright (C) 2015 Mattia Basaglia
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+#ifndef PIXEL_CAYMAN_DIALOG_INDEXED_COLORS_HPP
+#define PIXEL_CAYMAN_DIALOG_INDEXED_COLORS_HPP
+
+#include <QDialog>
+#include <QEvent>
+#include "ui_dialog_indexed_colors.h"
+
+class DialogIndexedColors : public QDialog, private Ui::DialogIndexedColors
+{
+    Q_OBJECT
+
+public:
+    explicit DialogIndexedColors(QWidget* parent = nullptr)
+    : QDialog(parent)
+    {
+        setupUi(this);
+    }
+
+protected:
+    void changeEvent(QEvent* event) override
+    {
+        if ( event->type() == QEvent::LanguageChange )
+        {
+            retranslateUi(this);
+        }
+
+        QDialog::changeEvent(event);
+    }
+};
+
+#endif // PIXEL_CAYMAN_DIALOG_INDEXED_COLORS_HPP
